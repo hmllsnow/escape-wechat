5aecc0e48e409ac66374e115683c25281f102842
miller <hmllsnow@gmail.com>
2024-06-14 15:35:34 +0800

    修改replyOpenAI使其支持个人聊天

--- bot/handlers/bot_handlers.js
+++ bot/handlers/bot_handlers.js
@@ -116,11 +116,44 @@ export async function replyOpenAI(message,bot) {
 
       await room.say(`${reply}\n  from internlm2`,talker);
     }catch(e){
-      console.log('coze error',e);
+      console.log('openai error',e);
       await room.say(`调用api出错了，请稍后再试吧\n${e.toString()}`,talker);
     }
     
     
+  }else{//如果是非群聊
+    console.log('这里是非群聊的replyOpenAI方法')
+    let content = await message.text()
+    const talker = await message.talker();
+    const receiver = message.to()
+    const userSelfName = bot.currentUser?.name() || bot.userSelf()?.name()
+    const receiverName = receiver?.name()
+    console.log('replayOpenAI----原始content='+content);
+    //content = content.replace('@' + receiverName, '').replace('@' + userSelfName, '').replace(/@[^,，：:\s@]+/g, '').trim()
+    //console.log(`hml----content=${content}`);
+    content = await removeMentions(message);
+    console.log('replayOpenAI----处理后的content='+content);
+    try{
+      console.log('openai config='+openaiconfig);
+      const openai = new OpenAI({
+        apiKey: `${openaiconfig.apikey}`,
+        baseURL: `${openaiconfig.baseurl}` ,
+      });
+      const completion = await openai.chat.completions.create({
+        messages: [{role: "user", content: `${content}` }],
+        //{ role: "system", content: "You are a helpful assistant." },{role: "user", content: "请做个自我介绍." }
+        model: `${openaiconfig.model}`,
+      });
+    
+      console.log(completion.choices[0]);
+      const reply = completion.choices[0].message.content;
+
+      await message.say(`${reply}\n  from LLM`,talker);
+    }catch(e){
+      console.log('openai error'+e.toString());
+      await message.say(`调用api出错了，请稍后再试吧\n${e.toString()}`,talker);
+    }
+
   }
 }
 
